01ed3238cc7c5826128eb8abc4aeee142d685098
pjcorona <corona.princejomer@gmail.com>
2018-10-31 14:07:12 +0800

    Add files via upload

---
+++ Password Manager WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Password_Manager_WebApp.MvcApplication" Language="C#" %>
---
+++ Password Manager WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Password_Manager_WebApp.Startup))]
+namespace Password_Manager_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
